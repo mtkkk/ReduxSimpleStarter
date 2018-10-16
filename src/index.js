201a49bc0fa386b014b67b6b82ad9af9946f8cdb
mtkkk <mutukax@hotmail.com>
2018-10-16 09:16:02 -0300

    Progresso

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyAPHhbSFAG2dqJF_Gx-0qVPSi5Rw_O7MPY";
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch("surfboards");
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos, 
+				selectedVideo: videos[0]
+			});
+			//In ES6 if the key and valeu are the same name, we can just use the variable name
+		});
+	}
+
+	render() {
+		//Debouce from lodash limits that this function can only run once every 300ms
+		const videoSearch = _.debounce( (term) => {this.videoSearch(term) }, 300);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		)
+	}
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
